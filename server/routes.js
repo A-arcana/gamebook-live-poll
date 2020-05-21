a0f58eb28f3219b1be698130de9137aecb9066d9
Pierre Godichal <godichal.pierre@gmail.com>
2020-05-21 16:12:14 +0200

    clean packages & files

--- server/routes.js
+++ server/routes.js
@@ -7,6 +7,10 @@ app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname + '/views/index.html'));
 });
 
+app.get('/node_modules/*', function (req, res) {
+	res.sendFile(path.join(__dirname + '/node_modules/' + req.params[0]));
+});
+
 app.get('/*', function (req, res) {
 	res.sendFile(path.join(__dirname + '/views/' + req.params[0]));
 });
