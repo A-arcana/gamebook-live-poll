e638762821476df0fe15cd06aa43aef12335a629
Pierre Godichal <godichal.pierre@gmail.com>
2020-05-22 01:08:31 +0200

    add possibility to select live poll, style++

--- views/js/ldvelh.js
+++ views/js/ldvelh.js
@@ -15,22 +15,41 @@ class LDVELH {
     lines = {};
     pictureTimeout = {};
     save() {
+        this.saveDOM();
         setCookie('ldvelh-app', JSON.stringify(this.config));
     }
+    saveDOM() {
+        this.config.liveId = $('#live-id').val();
 
+        this.config.bookNo = $('#book-no').val();
+        this.config.lastNo = $('#last-no').val();
+
+        this.config.tsv = $('#tsv').val();
+        this.config.cols.colId = $('#colId').val();
+        this.config.cols.colQuestion = $('#colQuestion').val();
+        this.config.cols.colOption = $('#colOption').val();
+    }
     load() {
         this.config = JSON.parse(getCookie('ldvelh-app'));
         this.loadDOM();
     }
     loadDOM() {
         $('#live-id').val(this.config.liveId);
+        $('#live-id').parent().toggleClass("is-filled", this.config.liveId);
 
         $('#book-no').val(this.config.bookNo);
+        $('#book-no').parent().toggleClass("is-filled", this.config.bookNo);
+
         $('#last-no').val(this.config.lastNo);
+        $('#last-no').parent().toggleClass("is-filled", this.config.lastNo);
 
         $('#tsv').val(this.config.tsv);
+        $('#tsv').parent().toggleClass("is-filled", this.config.tsv);
         $('#colId').val(this.config.cols.colId);
+        $('#colId').parent().toggleClass("is-filled", this.config.cols.colId > 0);
         $('#colQuestion').val(this.config.cols.colQuestion);
+        $('#colQuestion').parent().toggleClass("is-filled", this.config.cols.colQuestion > 0);
         $('#colOption').val(this.config.cols.colOption);
+        $('#colOption').parent().toggleClass("is-filled", this.config.cols.colOption > 0);
     }
 }
